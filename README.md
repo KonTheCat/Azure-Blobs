7d0c6277c53715eedd960d467152312fbeac21d1
Konstantin <konstantin.kaminskiy@outlook.com>
2023-07-01 12:49:37 -0400

    various changes to improve

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ A simple Python flask application to upload files to Azure Blob
 **Steps**
 
 1. Go to Azure management portal and create a Storage Account
-2. Find the api access key from the azure storage dashboard
-3. Copy paste the account name and api key in the python script
-4. Write a container name of your wish
-5. Run the requirements file to install the required dependencies
-6. Run the python flask app locally to get access no the upload page
+2. Provide the subscription ID, the storage account name, and the container name as environment variables
+3. Run the requirements file to install the required dependencies
+4. Run az login in the local console to log into Azure so that the flask app has authentication it can use using DefaultAzureCredential
+5. Run the python flask app locally to get access no the upload page
 
 Now you can select any file from your system and upload to the designated Azure Blob. Once the upload is completed, the system will return a url for future access.
 
--- blob.py
+++ blob.py
@@ -1,30 +1,47 @@
 import os
 from flask import Flask, request, redirect, url_for
-from werkzeug import secure_filename
-from azure.storage.blob import BlockBlobService
+from werkzeug.utils import secure_filename
+from azure.storage.blob import BlobServiceClient
 import string, random, requests
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.storage import StorageManagementClient
+from azure.identity import DefaultAzureCredential
 
 app = Flask(__name__, instance_relative_config=True)
 
-app.config.from_pyfile('config.py')
-account = app.config['ACCOUNT']   # Azure account name
-key = app.config['STORAGE_KEY']      # Azure Storage account access key  
-container = app.config['CONTAINER'] # Container name
+subscription = os.getenv("StorageAccountSubscriptionID")
+account = os.getenv("StorageAccountName")
+container = os.getenv("StorageContainerName")
 
-blob_service = BlockBlobService(account_name=account, account_key=key)
+def set_blob_data(data, storageAccountName, storageAccountKey, containerName, blobName):
+    blob_service_client = BlobServiceClient(account_url = f"https://{storageAccountName}.blob.core.windows.net", credential = storageAccountKey)
+    blob_client = blob_service_client.get_blob_client(container = containerName, blob = blobName)
+    blob_client.upload_blob(data, blob_type = "BlockBlob", overwrite = True)
+
+def get_primary_storage_account_key(storageAccountName, subscriptionID):
+    default_credential = DefaultAzureCredential(exclude_shared_token_cache_credential=True)
+    resource_client = ResourceManagementClient(default_credential, subscriptionID)
+    resourceList = resource_client.resources.list()
+    for item in resourceList:
+        if(item.type == 'Microsoft.Storage/storageAccounts' and item.name == storageAccountName):
+            resource_group_name = (item.id).split('/')[4]
+    storageClient = StorageManagementClient(default_credential, subscriptionID)
+    keys = storageClient.storage_accounts.list_keys(resource_group_name, storageAccountName)
+    primaryKey = keys.keys[0].value
+    return primaryKey
+
+key = get_primary_storage_account_key(account, subscription)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-    	file = request.files['file']
-    	filename = secure_filename(file.filename)
-    	fileextension = filename.rsplit('.',1)[1]
-        Randomfilename = id_generator()
-        filename = Randomfilename + '.' + fileextension
+        file = request.files['file']
+        filename = secure_filename(file.filename)
         try:
-            blob_service.create_blob_from_stream(container, filename, file)
+            set_blob_data(file, account, key, container, filename)
         except Exception:
-            print 'Exception=' + Exception 
+            print ('Exception=' + Exception) 
             pass
         ref =  'http://'+ account + '.blob.core.windows.net/' + container + '/' + filename
         return '''
@@ -44,8 +61,5 @@ def upload_file():
     </form>
     '''
 
-def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=81)
